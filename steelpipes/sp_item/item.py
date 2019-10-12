1b9972e322112158df764cb4296f95d1b51737d5
Muhammad Ahmad <mohammad.ahmad.zulfiqar1@gmail.com>
2019-10-12 17:45:47 +0500

    Bug fixed

--- steelpipes/sp_item/item.py
+++ steelpipes/sp_item/item.py
@@ -10,21 +10,31 @@ def pipe_custom_name(self,cdt):
             
             if 'Size' in attribute_name:
                 item_size_um = str(d.attribute_value)
-                item_code_temp += '-{0} INCH'.format(item_size_um)
+                temp_size = ''
                 for character in item_size_um:
-                    if character != '/':
-                        barcode += character
+                    if character != '/' and character!=' ':
+                        temp_size += character
+                item_code_temp += '-{0} INCH'.format(temp_size)
+                barcode += temp_size
             elif 'Thickness' in attribute_name:
                 item_thickness_um = str(d.attribute_value)
-                if len(item_thickness_um)!=4:
-                    item_thickness_um +='0'
-                item_code_temp += "-{0} MM".format(item_thickness_um)
-                barcode += item_thickness_um
+                temp_thickness = ''
+                for character in item_thickness_um:
+                    if character != ' ':
+                        temp_thickness += character
+                if len(temp_thickness)!=4:
+                    temp_thickness +='0'
+                item_code_temp += "-{0} MM".format(temp_thickness)
+                barcode += temp_thickness
 
             elif 'Length' in attribute_name:
                 item_length_um = str(d.attribute_value)
-                item_code_temp += "-{0} FT".format(item_length_um)
-                barcode += item_length_um
+                temp_length = ''
+                for character in item_length_um:
+                    if character != ' ':
+                        temp_length += character
+                item_code_temp += "-{0} FT".format(temp_length)
+                barcode += temp_length
         if self.barcodes:
             has_barcode = 0
             for d in self.barcodes:
