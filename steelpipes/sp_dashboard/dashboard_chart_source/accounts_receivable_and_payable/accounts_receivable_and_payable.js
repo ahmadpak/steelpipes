daf3360ace0316786df463fe9d2d0f39d3b674dd
Muhammad Ahmad <mohammad.ahmad.zulfiqar1@gmail.com>
2019-10-24 14:15:18 +0500

    dashboard Receivable and payable source init

---
+++ steelpipes/sp_dashboard/dashboard_chart_source/accounts_receivable_and_payable/accounts_receivable_and_payable.js
@@ -0,0 +1,22 @@
+frappe.provide('frappe.dashboards.chart_sources');
+
+frappe.dashboards.chart_sources["Accounts receivable and payable"] = {
+	method: "steelpipes.sp_dashboard.dashboard_chart_source.accounts_receivable_and_payable.accounts_receivable_and_payable.get",
+	filters: [
+		{
+			fieldname: "company",
+			label: __("Company"),
+			fieldtype: "Link",
+			options: "Company",
+			default: frappe.defaults.get_user_default("Company"),
+			reqd: 1
+		},
+		{
+			fieldname: "account",
+			label: __("Account"),
+			fieldtype: "Link",
+			options: "Account",
+			reqd: 1
+		},
+	]
+};
