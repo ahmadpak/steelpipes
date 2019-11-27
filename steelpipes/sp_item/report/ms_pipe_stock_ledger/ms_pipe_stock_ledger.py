0da78e7351b7ac6025d8e98f6b779b0e227b0cd3
Muhammad Ahmad <mohammad.ahmad.zulfiqar1@gmail.com>
2019-11-27 12:22:36 +0500

    filter for MS Pipe Stock Ledger

--- steelpipes/sp_item/report/ms_pipe_stock_ledger/ms_pipe_stock_ledger.py
+++ steelpipes/sp_item/report/ms_pipe_stock_ledger/ms_pipe_stock_ledger.py
@@ -6,4 +6,35 @@ from __future__ import unicode_literals
 
 def execute(filters=None):
 	columns, data = [], []
+	columns = get_columns(filters)
 	return columns, data
+
+
+def get_columns(filters):
+	if filters.get('warehouse'):
+		columns = [
+		{"label": _("Date"), "fieldname": "date", "fieldtype": "Datetime", "width": 95},
+		{"label": _("Item"), "fieldname": "item_code", "fieldtype": "Link", "options": "Item", "width": 200},
+		{"label": _("Est. Weight(KG)"), "fieldname": "estimate_weight", "fieldtype": "Float","precision":2, "width": 60},
+		{"label": _("Scale Weight(KG)"), "fieldname": "scale_weight", "fieldtype": "Float","precision":2, "width": 60},
+		{"label": _("Total Scale Weight(KG)"), "fieldname": "total_scale_weight", "fieldtype": "Float","precision":2, "width": 100},
+		{"label": _("Qty"), "fieldname": "actual_qty", "fieldtype": "Float","precision":2, "width": 60, "convertible": "qty"},
+		{"label": _("Balance Qty"), "fieldname": "qty_after_transaction", "fieldtype": "Float","precision":2, "width": 100, "convertible": "qty"},
+		{"label": _("Voucher Type"), "fieldname": "voucher_type", "width": 110},
+		{"label": _("Voucher #"), "fieldname": "voucher_no", "fieldtype": "Dynamic Link", "options": "voucher_type", "width": 150},
+		]
+		return columns	
+	columns = [
+		{"label": _("Date"), "fieldname": "date", "fieldtype": "Datetime", "width": 95},
+		{"label": _("Item"), "fieldname": "item_code", "fieldtype": "Link", "options": "Item", "width": 200},
+		{"label": _("Warehouse"), "fieldname": "warehouse", "fieldtype": "Link", "options": "Warehouse", "width": 100},
+		{"label": _("Est. Weight(KG)"), "fieldname": "estimate_weight", "fieldtype": "Float","precision":2, "width": 60},
+		{"label": _("Scale Weight(KG)"), "fieldname": "scale_weight", "fieldtype": "Float","precision":2, "width": 60},
+		{"label": _("Total Scale Weight(KG)"), "fieldname": "total_scale_weight", "fieldtype": "Float","precision":2, "width": 100},
+		{"label": _("Qty"), "fieldname": "actual_qty", "fieldtype": "Float","precision":2, "width": 100, "convertible": "qty"},
+		{"label": _("Balance Qty"), "fieldname": "qty_after_transaction", "fieldtype": "Float","precision":2, "width": 100, "convertible": "qty"},
+		{"label": _("Voucher Type"), "fieldname": "voucher_type", "width": 110},
+		{"label": _("Voucher #"), "fieldname": "voucher_no", "fieldtype": "Dynamic Link", "options": "voucher_type", "width": 150},
+	]
+
+	return columns
