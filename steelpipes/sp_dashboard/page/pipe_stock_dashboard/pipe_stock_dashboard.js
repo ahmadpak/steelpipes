6463cc45d08c4121a30e86b36f60def6871f15bf
Muhammad Ahmad <ahmad@dev.mshome.net>
2019-12-31 05:43:59 +0000

    removed page pipe stock dashboard

--- steelpipes/sp_dashboard/page/pipe_stock_dashboard/pipe_stock_dashboard.js
+++
@@ -1,8 +0,0 @@
-frappe.pages['pipe-stock-dashboard'].on_page_load = function(wrapper) {
-	var page = frappe.ui.make_app_page({
-		parent: wrapper,
-		title: 'Pipes Production Insight',
-		single_column: true
-	});
-	
-}
