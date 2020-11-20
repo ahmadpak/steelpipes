857dc9206b0ec22029a51b81e7b772a3411ddca2
afifaa0 <afifasaleem77@gmail.com>
2020-11-20 17:31:38 +0500

    update:   fetch contact from customer

--- steelpipes/sp_payment_entry/sp_payment_entry.js
+++ steelpipes/sp_payment_entry/sp_payment_entry.js
@@ -1,4 +1,34 @@
 frappe.ui.form.on('Payment Entry', {
+    party: function(frm) {
+        if (frm.doc.party) {
+            frappe.db.get_list('Contact', {
+            filters : {
+                'status': 'Open'
+            }})
+            .then( r => {
+                for (let i in r) {
+                    frappe.db.get_doc('Contact', r[i].name)
+                    .then ( doc => {
+                        if (doc.links) {
+                            if (doc.links[0].link_doctype === frm.doc.party_type && 
+                                doc.links[0].link_name === frm.doc.party) {
+                                    frm.set_value('contact_person', doc.name)
+                                }
+                            }
+                        })
+                    }
+            })
+        }
+        // frappe.db.get_value('Dynamic Link', {
+        //     'link_doctype': frm.doc.party_type,
+        //     'link_name': frm.doc.party,
+        //     'parenttype': 'Contact'
+        // }, 'parent')
+        // .then(r => {
+        //     frm.set_value('contact_person', r.parent)
+        // })
+    },
+
     contact_person: function(frm) {
         if (frm.doc.contact_person) {
             frappe.call('steelpipes.sp_payment_entry.sp_payment_entry.set_phone',
