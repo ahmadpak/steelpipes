0bcf1764d0e851821dcdb06c1ba2d0d1f112d25d
afifaa0 <afifasaleem77@gmail.com>
2020-11-21 11:11:37 +0500

    fix:   payment entry

--- steelpipes/sp_payment_entry/sp_payment_entry.js
+++ steelpipes/sp_payment_entry/sp_payment_entry.js
@@ -1,32 +1,14 @@
 frappe.ui.form.on('Payment Entry', {
     party: function(frm) {
         if (frm.doc.party) {
-            frappe.db.get_list('Contact', {
-            filters : {
-                'status': 'Open'
-            }})
-            .then( r => {
-                for (let i in r) {
-                    frappe.db.get_doc('Contact', r[i].name)
-                    .then ( doc => {
-                        if (doc.links) {
-                            if (doc.links[0].link_doctype === frm.doc.party_type && 
-                                doc.links[0].link_name === frm.doc.party) {
-                                    frm.set_value('contact_person', doc.name)
-                                }
-                            }
-                        })
-                    }
+            frappe.call('steelpipes.sp_payment_entry.sp_payment_entry.get_contract',
+            {party_type: frm.doc.party_type, party: frm.doc.party})
+            .then (r => {
+                if (r.message) {
+                    frm.set_value('contact_person', r.message)
+                }
             })
         }
-        // frappe.db.get_value('Dynamic Link', {
-        //     'link_doctype': frm.doc.party_type,
-        //     'link_name': frm.doc.party,
-        //     'parenttype': 'Contact'
-        // }, 'parent')
-        // .then(r => {
-        //     frm.set_value('contact_person', r.parent)
-        // })
     },
 
     contact_person: function(frm) {
